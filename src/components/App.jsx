ecd9751b966f88d8693329a6dba6a71cf0f5dd88
juliabykovaa <97822247+juliabykovaa@users.noreply.github.com>
2023-05-27 16:22:26 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import ContactForm from '../components/ContactForm/ContactForm';
+import { ContactBook } from './ContactBook/ContactBook';
+import { Filter } from './FilterContactBook/FilterContactBook';
+import { nanoid } from 'nanoid';
+import { Container, Header } from './App.styled';
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const savedContact = JSON.parse(localStorage.getItem('phone'));
+    if (savedContact) {
+      setContacts(savedContact);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('phone', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const addToContacts = ({ name, number }) => {
+    if (contacts.some(contact => contact.name === name)) {
+      alert('This contact is already in your contact book');
+    } else {
+      const contactToAdd = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      setContacts(prevContacts => [contactToAdd, ...prevContacts]);
+    }
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getFilteredContacts = () => {
+    const filterNormalized = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterNormalized)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Container>
+        <Header>Phonebook</Header>
+        <ContactForm onSubmit={addToContacts} contacts={contacts} />
+
+        <Header>Contacts</Header>
+        <Filter value={filter} onChange={changeFilter} />
+        {filteredContacts.length ? (
+          <ContactBook
+            contacts={filteredContacts}
+            onDeleteContact={deleteContact}
+          />
+        ) : (
+          <p>No contacts</p>
+        )}
+      </Container>
+    </>
   );
-};
+}
+
+export default App;
---
+++ src/components/FilterContactBook/FilterContactBook.jsx
@@ -0,0 +1,14 @@
+import { React } from 'react';
+import PropTypes from 'prop-types';
+
+import { Input } from './FilterContactBook.styled';
+
+export const Filter = ({ value, onChange }) => (
+  <label>
+    <Input type="text" placeholder="Search" value={value} onChange={onChange} />
+  </label>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string,
+};
